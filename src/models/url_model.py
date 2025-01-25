3cadb57eb7b96285bc89e350f2ccf3650b19ed22
rinal <rinalditoryanari@gmail.com>
2025-01-25 22:53:05 +0700

    fix: moving the non user submission attribute to the full model

--- src/models/url_model.py
+++ src/models/url_model.py
@@ -2,19 +2,18 @@ from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
 from sqlalchemy import Text
 
-from .model import Base
+from .base_model import Base
 
 
-class UrlBase(SQLModel):
+class UrlBase(SQLModel): # Base Model is only for user submitted data
     title: Optional[str] = Field(default=None, nullable=True)
     original_url: str = Field(index=True, sa_type=Text, max_length=1048)
-    shortened_url: str = Field(index=True, unique=True)
+    short_code: str = Field(index=True, unique=True)
+    clicks : int = Field(default=0)
+
+
+class UrlFull(UrlBase, Base): # The rest of the model
     is_active: bool = Field(default=True)
-
-    # visits: Optional["VisitModel"] = Relationship(back_populates="url")
-
-
-class UrlFull(UrlBase, Base):
     pass
 
 

d8085ca4c8cbc48dc4c9a60f54d5b33195aff150
rinal <rinalditoryanari@gmail.com>
2025-01-28 12:05:16 +0700

    docs: rewrite the docs in the url mdoel

--- models/url_model.py
+++ models/url_model.py
@@ -5,14 +5,16 @@ from sqlalchemy import Text
 from .base_model import Base
 
 
-class UrlBase(SQLModel): # Base Model is only for user submitted data
+class UrlBase(SQLModel):
+    # Base Model is only for user submitted data
     title: Optional[str] = Field(default=None, nullable=True)
     original_url: str = Field(index=True, sa_type=Text, max_length=1048)
     shortlink: str = Field(index=True, unique=True)
     clicks : int = Field(default=0)
 
 
-class UrlFull(UrlBase, Base): # The rest of the model
+class UrlFull(UrlBase, Base):
+    # Manage by the systems, like relationship
     is_active: bool = Field(default=True)
     pass
 

a6f366308dc3294f104f5e0cdac704b099b21bc4
rinal <rinalditoryanari@gmail.com>
2025-01-28 12:12:04 +0700

    feat: add vlaidator for urlbase

--- configs/settings.py
+++ configs/settings.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+from pydantic import field_validator
 
 load_dotenv()  # Load environment variables from .env file
 
@@ -9,9 +10,18 @@ class Settings(BaseSettings):
     shortlink_length: int = 6
     URL_BASE: str
 
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+
+    @field_validator("URL_BASE")
+    # Ensuring the url base always ends with "/"
+    def validate_url_base(cls, value):
+        if not value.endswith("/"):
+            value += "/"
+        return value
+
 
 
 settings = Settings()  # Load values from .env file

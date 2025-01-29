f0733205590226bbe8a29cfb9651612a3266a39d
rinal <rinalditoryanari@gmail.com>
2025-01-29 16:09:10 +0700

    fix: problem migration alembic revision for datetime

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,22 @@
 from datetime import datetime
 from uuid import UUID, uuid4
 
-from sqlalchemy import Column, func
+from sqlalchemy import Column
 from sqlalchemy.orm import declared_attr
 from sqlmodel import Field
 from sqlmodel import SQLModel as _SQLModel
 from stringcase import snakecase
 
-from utils.datetime import Datetime
-
+from utils.datetime import Datetime  # Ensure this import is correct
 
 class SQLModel(_SQLModel):
     @declared_attr  # type: ignore
     def __tablename__(cls) -> str:
         return snakecase(cls.__name__)
 
-
 class Base(SQLModel):
     id: UUID = Field(default_factory=uuid4, primary_key=True, index=True)
-    created_at: datetime | None = Field(default_factory=datetime.now, sa_column=Column(Datetime, nullable=False))
-    updated_at: datetime | None = Field(default=None,
-                                        sa_column=Column(Datetime, nullable=False, onupdate=datetime.utcnow))
+    created_at: datetime | None = Field(default_factory=datetime.now, sa_column=Column(Datetime(), nullable=False))
+    updated_at: datetime | None = Field(default_factory=datetime.now, sa_column=Column(Datetime(), nullable=False, onupdate=datetime.utcnow))
+
+    

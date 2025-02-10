89a539f626bcbc153acc6434b7853c64f329287d
rinal <rinalditoryanari@gmail.com>
2025-02-10 09:00:54 +0700

    fix: remove pass at UrlFull

--- models/url_model.py
+++ models/url_model.py
@@ -4,20 +4,16 @@ from sqlalchemy import Text
 
 from .base_model import Base
 
-
 class UrlBase(SQLModel):
-    # Base Model is only for user submitted data
+    # Base Model is where user can tinkered with
     title: Optional[str] = Field(default=None, nullable=True)
     original_url: str = Field(index=True, sa_type=Text, max_length=1048)
     shortlink: str = Field(index=True, unique=True)
-    clicks : int = Field(default=0)
-
+    clicks: int = Field(default=0)
 
 class UrlFull(UrlBase, Base):
     # Manage by the systems, like relationship
     is_active: bool = Field(default=True)
-    pass
-
 
 class Url(UrlFull, table=True):
     pass

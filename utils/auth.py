e31679ebe3446322b8782f11eb431dc34630a871
rinal <rinalditoryanari@gmail.com>
2025-02-10 21:26:40 +0700

    feat: verify password

--- utils/auth.py
+++ utils/auth.py
@@ -5,5 +5,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class Auth:
 
+    def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(plain_password, hashed_password)
+
     def get_hash_password(password: str) -> str:
         return pwd_context.hash(password)

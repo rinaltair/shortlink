ded5a14f1ece3579125161944d8cfbf2aaab854f
rinal <rinalditoryanari@gmail.com>
2025-02-11 09:44:13 +0700

    refactor: move the env file outside configuration

--- configs/settings.py
+++ configs/settings.py
@@ -1,19 +1,24 @@
+from typing import List
+
 from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
 from pydantic import field_validator
+from pydantic_settings import BaseSettings
 
 load_dotenv()  # Load environment variables from .env file
 
 
+class BaseConfig:
+    """ .env file configuration class."""
+    env_file = ".env"
+    env_file_encoding = "utf-8"
+
+
 class Settings(BaseSettings):
+    """Application settings class."""
     DB_CONFIG: str
     shortlink_length: int = 6
     URL_BASE: str
-    RESTRICTED_SHORTLINK: list[str] = ['api', 'auth']
-
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    RESTRICTED_SHORTLINK: List[str] = ['api', 'auth']
 
     @field_validator("URL_BASE")
     # Ensuring the url base always ends with "/"
@@ -22,6 +27,7 @@ class Settings(BaseSettings):
             value += "/"
         return value
 
+    class Config(BaseConfig): pass
 
 
 settings = Settings()  # Load values from .env file

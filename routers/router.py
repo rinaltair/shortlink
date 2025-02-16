6293154da3f9f4c997f8a53d99d99679a482b2f0
Rinaldito <rinalditorynari@gmail.com>
2025-02-16 10:56:55 +0700

    feat: add route guard

--- routers/router.py
+++ routers/router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
+from fastapi.params import Depends
 
+from utils.jwt import get_current_user
 from .endpoints import url, redirect, user, auth
 
 api_router = APIRouter()  # dependencies=[Depends(Permission().is_allowed_user)])
 
 api_router.include_router(redirect, prefix='', tags=['redirect'])
 api_router.include_router(auth, prefix='/api/auth', tags=['auth'])
-api_router.include_router(user, prefix='/api/user', tags=['user'])
-api_router.include_router(url, prefix='/api/shortlink', tags=['shortlink'])
+api_router.include_router(user, prefix='/api/user', tags=['user'], dependencies=[Depends(get_current_user)])
+api_router.include_router(url, prefix='/api/shortlink', tags=['shortlink'], dependencies=[Depends(get_current_user)])

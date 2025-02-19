ebb3c01b372e7129c94c72cd8627747ec4565e9f
rinaltair <rinalditoryanari@gmail.com>
2025-02-19 09:53:31 +0700

    add authorize url

---
+++ utils/authorize.py
@@ -0,0 +1,26 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from uuid import UUID
+
+from dependencies.database import get_db
+from dependencies.auth import auth_user
+from models.user_model import User
+from repositories import UrlRepositories
+
+async def authorize_url(
+    id: UUID,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_user)
+) -> None:
+    url = await UrlRepositories(db).get(id)
+    if not url: raise LookupError('Shortlink not found')
+
+    if user.role == "admin":
+        return id
+    if user.role == "user" and user.id == url.user_id:
+        return id
+    
+    if user.id == url.user_id:
+        return id
+
+    raise PermissionError("You are not authorized to perform this action")

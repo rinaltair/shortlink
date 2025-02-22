a2d7953669cea74bd944b6377bbe231c2685acd0
Rinaldito <rinalditorynari@gmail.com>
2025-02-22 22:46:25 +0700

    change user management to all

--- routers/router.py
+++ routers/router.py
@@ -2,11 +2,10 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 
 from .endpoints import url, redirect, user, auth
-from dependencies.auth import auth_user
-
+from dependencies.auth import auth_user, auth_active
 api_router = APIRouter()  # dependencies=[Depends(Permission().is_allowed_user)])
 
 api_router.include_router(redirect, prefix='', tags=['redirect'])
 api_router.include_router(auth, prefix='/api/auth', tags=['auth'])
-api_router.include_router(user, prefix='/api/user', tags=['user'], dependencies=[Depends(auth_user)])
+api_router.include_router(user, prefix='/api/user', tags=['user'], dependencies=[Depends(auth_active)])
 api_router.include_router(url, prefix='/api/shortlink', tags=['shortlink'], dependencies=[Depends(auth_user)])

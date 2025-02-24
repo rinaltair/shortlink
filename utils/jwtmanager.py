df6d9a2bd1d469c76e69d2b0713a3e535d72a574
= <=>
2025-02-24 11:42:55 +0700

    upgrade pyjwt (idk why i got the older one)

--- utils/jwtmanager.py
+++ utils/jwtmanager.py
@@ -2,7 +2,7 @@ from datetime import timedelta, datetime, timezone
 
 from fastapi import HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jwt import JWT, supported_key_types, AbstractJWKBase
+from jwt import PyJWT
 
 from configs.settings import settings
 from schemas.token import TokenData
@@ -14,7 +14,7 @@ from schemas.token import TokenData
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
-jwt = JWT()
+jwt = PyJWT()
 
 credentials_exception = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
@@ -30,18 +30,18 @@ class JWTManager():
         self.access_token_expire = settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES
 
 
-    async def key_instance(self) -> AbstractJWKBase:
-        secret_bytes = self.secret_key.encode()
-        return supported_key_types()['oct'](secret_bytes)
+    # async def key_instance(self) -> AbstractJWKBase:
+    #     secret_bytes = self.secret_key.encode()
+    #     return supported_key_types()['oct'](secret_bytes)
 
 
-    async def create_access_token(self, data: dict, expires_delta: timedelta | None) -> str:
+    async def create_access_token(self, data: dict, expires_delta: timedelta | None = None) -> str:
         """Create a new access token for a given data and expiration delta"""
         to_encode = data.copy()
         expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=15))
         to_encode.update({"exp": int(expire.timestamp())})
-        key =await self.key_instance()
-        encode_jwt = jwt.encode(payload=to_encode, key= key, alg=self.algorithm)
+        # key =await self.key_instance()
+        encode_jwt = jwt.encode(payload=to_encode, key= self.secret_key, algorithm=self.algorithm)
         return encode_jwt
 
 
@@ -49,16 +49,14 @@ class JWTManager():
         """Verify the token and return the data"""
         try:
             payload = jwt.decode(
-                token=token, 
-                key=await self.key_instance(), 
-                algorithms=self.algorithm, 
-                options= { 
-                    "require":["exp"], 
-                    "verify_exp":True
-                }
+                token, 
+                key=self.secret_key, 
+                algorithms=self.algorithm,
+                require=["exp"],
+                verify_exp=True,
             )
             username: str = payload.get("sub")
-            if username is None: raise Exception("Invalid token data")
+            if username is None: raise Exception("Invalid token data: 'sub' claim is missing")
             token_data = TokenData(username=username)
             return token_data
         except Exception as e:

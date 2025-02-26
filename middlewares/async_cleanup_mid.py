6762a9fec8ac673f3f34b8a7f12b7b0b9a585f0b
= <=>
2025-02-26 13:20:50 +0700

    add ascync cleanup middleware

--- middlewares/async_cleanup_mid.py
+++ middlewares/async_cleanup_mid.py
@@ -1,9 +1,9 @@
 from fastapi import Request
-
 from starlette.middleware.base import BaseHTTPMiddleware
 
 class AsyncCleanupMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         response = await call_next(request)
-        await request.state.db.aclose()  # If using request-scoped db
+        if hasattr(request.state, 'db'):
+            await request.state.db.close()  # Close the database connection
         return response
